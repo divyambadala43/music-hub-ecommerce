59537958d058de0b3401cbc2c3a186cb1f36e785
divyambadala43 <divyambadala43@gmail.com>
2022-04-24 12:56:53 +0530

    UPDATE: Layout.js and _app.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import Head from "next/head";
+import React from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="layout">
+      <Head>
+        <title>Music Hub</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
