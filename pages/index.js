a1975b0f9d1568fca54054c5137e37e2c92e2e7d
divyambadala43 <divyambadala43@gmail.com>
2022-04-24 11:03:38 +0530

    ADD: HeroBanner.js

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,13 @@ const Home = () => {
   return (
     <>
       HeroBanner
-      <div>
+      <div className="products-heading">
         <h2>Best selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
-
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
       Footer
     </>
   );
